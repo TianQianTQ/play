c7c6c01d1b771e554fb6984684bb9966ac85eeb3
TianQianTQ <1114580464@qq.com>
2021-09-19 22:43:57 +0800

    feat:  09-19 深浅拷贝

--- arith/js基础实现/浅拷贝深拷贝.js
+++ arith/js基础实现/浅拷贝深拷贝.js
@@ -1,7 +1,9 @@
 // 浅拷贝： concat() Object.assign();
 // 深拷贝：
 // 1、JSON.parse(JSON.stringify()) // 正常的json情况下 （对象的方法可能为undefined）
+// 问题：会忽略undefined Symbol,不能序列化函数，不能解决循环引用的对象，不能处理date和正则
 // 2、递归方法
+注意点：1、递归实现深拷贝、参数校验、数组的兼容、循环引用的处理（使用map保存查询）
 
 function deepClone (data) {
   if (typeof data !== 'object'  || data == null) return data;
@@ -44,6 +46,7 @@ if (typeof Object.assign2 !== 'function') {
         // 赋值对象 值放在目标对象to上
         let source = arguments[i];
         if (source !== null) {
+          // for-in循环遍历出所有可枚举的自由有属性
           for (let key in source) {
             // 检查下source上是否含有属性key 过滤原型链上的属性
             if (Object.prototype.hasOwnProperty.call(source, key)) {
@@ -58,5 +61,70 @@ if (typeof Object.assign2 !== 'function') {
     configurable: true,  // 属性描述符是否可被删除
   });
 }
+
+
+实现深拷贝:
+
+// 判断是否是对象
+function isObject(obj) {
+  return typeof obj === 'object' && obj !== null;
+}
+
+
+function cloneDeep(source, hash = new WeakMap()) {
+  // 不是对象直接返回自身
+  if (!isObject(source)) return source;
+  // 查询是否存在引用
+  if (hash.has(source)) return hash.get(source);
+  // 兼容数组
+  let target = Array.isArray(source) ? [] : {};
+  hash.set(source, target); //  添加哈希表设值
+
+  let symKeys = Object.getOwnPropertySymbols(source); //查找 Symbol
+  if (symKeys.length) {
+    symKeys.forEach(symKey => {
+      if (isObject(source[symKey])) {
+          target[symKey] = cloneDeep4(source[symKey], hash); 
+      } else {
+          target[symKey] = source[symKey];
+      }    
+    });
+  }
+
+  for (let key in source) {
+    if (Object.prototype.hasOwnProperty.call(source, key)) {
+      if (isObject(source[key])) {
+        target[key] = cloneDeep(source[key], hash);
+      } else {
+        target[key] = source[key];
+      }
+    }
+  }
+  return target;
+}
+
+// weakMap与map的区别：
+// weakMap拥有set和get方法
+// weakMap只接受对象作为键名(null)除外，不接受其他类型 的值作为键名 
+// weakMap的键名所指向的对象，不计入垃圾回收机制
+// weakMap的设计目的：在某个对象上面存放一些数据，会形成对于这个对象的引用
+// WeakMap 就是为了解决这个问题而诞生的，它的键名所引用的对象都是弱引用，即垃圾回收机制不将该引用考虑在内。
+// 因此，只要所引用的对象的其他引用都被清除，垃圾回收机制就会释放该对象所占用的内存。也就是说，一旦不再需要，WeakMap 里面的键名对象和所对应的键值对会自动消失，不用手动删除引用。
+// 如果你要往对象上添加数据，又不想干扰垃圾回收机制，就可以使用 WeakMap。
+
+// weakMap替代方案- 数组查询;
+// [
+//   {
+//     source: source,
+//     target: target
+//   }
+// ]
+
+// 检测symbol 
+// 1、Object.getOwnPropertySymbols(...)
+// 2、Reflect.ownKeys(...)
+
+// 破解递归爆栈： 循环 while(loopList.length) { 广度优先遍历  loopList.push（xx） }
+
 
 
